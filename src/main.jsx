e25e65feb6c6bed2407fd9647e6b43379573320b
Application-Development-Department <app.development@mects.co.mz>
2026-06-16 10:56:27 +0200

    App changed to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './index.css'   // Your original Tailwind/Shadcn file
 import './global.css'  // Your newly created, conflict-free portal styles
 
